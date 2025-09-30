d04f6b95a81912e97bade78aae8963c803df6af6
Willian M. <107873380+0Willp@users.noreply.github.com>
2025-09-30 02:55:21 +0000

    update src

--- src/app.py
+++ src/app.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 
 import asyncio
+from websockets.exceptions import ConnectionClosedOK
+
 
 from websockets.asyncio.server import serve
 
 
 async def handler(websocket):
-    while True:
-        message = await websocket.recv()
+    async for message in websocket:
         print(message)
 
 
 async def main():
-    async with serve(handler, "", 8001) as server:
+    async with serve(handler, "", 8001) as server: 
         await server.serve_forever()
 
 
